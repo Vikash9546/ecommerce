23870d8573eb557cdc1d265902109d0e99815244
vikash kumar <vikashkumarmystyle@gmail.com>
2026-01-30 15:29:39 +0530

    Remove folder-name

--- backend/src/routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addToCart, getCart } from "../controllers/cartController.js";
-
-const router = express.Router();
-
-router.post("/add", addToCart);
-router.get("/:userId", getCart);
-
-export default router;
