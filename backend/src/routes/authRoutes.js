d172d294be4e9897d4ee80bed4e0a0ec97f0c893
vikash kumar <vikashkumarmystyle@gmail.com>
2026-02-11 16:49:55 +0530

    fix: restore missing express import in authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,3 +1,4 @@
+import express from "express";
 import { register, login, getProfile, updateProfile } from "../controllers/authController.js";
 import protect from "../middleware/authMiddleware.js";
 
